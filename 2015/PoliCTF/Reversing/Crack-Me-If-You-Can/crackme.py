f34f54ec14b267d398ebc3cb28d4812313560a5c
Alpackers <teamalpackers@gmail.com>
2015-07-15 16:03:32 -0500

    Update

---
+++ 2015/PoliCTF/Reversing/Crack-Me-If-You-Can/crackme.py
@@ -0,0 +1,42 @@
+def ca(text):
+	return text.replace("aa","ca")
+
+def cb(text):
+	return text.replace("aat","his")
+
+def cc(text):
+	return text.replace("buga","Goo")
+
+def cd(text):
+	return text.replace("spdgj","yb%e")
+
+def ba(text):
+	return text.replace("c","a")
+
+def bb(text):
+	return text.replace("%","")
+
+def bc(text):
+	return text.replace("[","")
+
+def bd(text):
+	return text.replace("]","")
+
+def be(text):
+	return text.replace("\\{","")
+
+def bf(text):
+	return text.replace("\\}","")
+
+def bg(text):
+	return text.replace("c","f")
+
+def bh(text):
+	return text.replace("R","f")
+
+def bi(text):
+	return text.replace("=","_")
+
+flag="[[c%l][c{g}[%{%Mc%spdgj=]T%aat%=O%bRu%sc]c%ti[o%n=Wcs%=No[t=T][hct%=buga[d=As%=W]e=T%ho[u%[%g]h%t[%}%"
+
+print ca(ba(bb(bc(bd(bg(bh(be(bf(bi(cc(cb(cd(flag)))))))))))))
