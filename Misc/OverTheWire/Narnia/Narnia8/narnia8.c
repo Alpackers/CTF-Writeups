dd2faeef9036aee823792383f2b0f6069d00b0aa
Alpackers <teamalpackers@gmail.com>
2015-07-24 20:28:16 -0500

    Update attachments

---
+++ Misc/OverTheWire/Narnia/Narnia8/narnia8.c
@@ -0,0 +1,45 @@
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// gcc's variable reordering fucked things up
+// to keep the level in its old style i am 
+// making "i" global unti i find a fix 
+// -morla 
+int i; 
+
+void func(char *b){
+	char *blah=b;
+	char bok[20];
+	//int i=0;
+	
+	memset(bok, '\0', sizeof(bok));
+	for(i=0; blah[i] != '\0'; i++)
+		bok[i]=blah[i];
+
+	printf("%s\n",bok);
+}
+
+int main(int argc, char **argv){
+        
+	if(argc > 1)       
+		func(argv[1]);
+	else    
+	printf("%s argument\n", argv[0]);
+
+	return 0;
+}
